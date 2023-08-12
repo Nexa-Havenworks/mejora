c50eadb092911a2432fa2b14974ba9d80e19f415
Otto G <5030059+ottodevs@users.noreply.github.com>
2023-08-12 22:59:36 +0200

    implement profile creation page

---
+++ app/profile/create/page.tsx
@@ -0,0 +1,10 @@
+import { ProfileForm } from '@/components/forms/ProfileForm'
+
+export default function CreateProfile() {
+    return (
+        <div>
+            <h1 className='mb-8 text-5xl font-semibold'>Creating a profile</h1>
+            <ProfileForm />
+        </div>
+    )
+}
